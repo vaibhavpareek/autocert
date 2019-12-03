3e95472e14cef127c0f00da8bbf2eddd014430b5
vaibhavpareek <vaibhavpareekvp7@gmail.com>
2019-12-03 22:33:08 +0530

    code uploaded

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # autocert
-AutoCert is a small script which can be used to create n number of certificates from the template certificate specially with the name.
+AutoCert is a small script which is created to make n number of certificates importing csv files.
+
+Autocert is made to automate the boring stuff of automating the name change to create certificates.
+
+Follow the guideline to run it swiftly:
+
+1. Add one csv file containing email address and name.
+2. Add Template certificate
+3. Manually you have to find the X index and Y index where you want to insert the name.
+4. Then decide you want to save the pdf via the name or email id.
+5. Everything is fine , you can create n number of certificates just by automation.
---
+++ autopdf.py
@@ -0,0 +1,38 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+import csv
+reg = input("Enter the CSV file location : ")
+print("\n")
+reader = csv.DictReader(open(reg))
+pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
+j = 0
+cert = input("Enter the Certificate Location with Name")
+for i in reader:
+	if(i['Name']!=""):
+		packet = io.BytesIO()
+		# create a new PDF with Reportlab
+		can = canvas.Canvas(packet, pagesize=letter)
+		can.setFont("Vera",20)
+		can.drawString(363, 210, i['Name'])
+		can.save()
+		#move to the beginning of the StringIO buffer
+		packet.seek(0)
+		new_pdf = PdfFileReader(packet)
+		# read your existing PDF
+		email = i['Username']+".pdf"
+		existing_pdf = PdfFileReader(open(cert, "rb"))
+		output = PdfFileWriter()
+		# add the "watermark" (which is the new pdf) on the existing page
+		page = existing_pdf.getPage(0)
+		page.mergePage(new_pdf.getPage(0))
+		output.addPage(page)
+		# finally, write "output" to a real file
+		outputStream = open(email, "wb")
+		output.write(outputStream)
+		outputStream.close()
+		j = j+1
+		print("Certificate Done : "+str(j))
